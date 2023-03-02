71bb4ca927846e90c828ed75250ee9368dd21fd7
cheeseNA <kurocat2000@gmail.com>
2023-03-03 03:14:54 +0900

    Add routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { Auth } from './components/Auth';
 import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Items } from './components/Items';
 
 function App() {
   return (
-    <div>
-      <AuthProvider>
-        <Auth />
-      </AuthProvider>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Items />} />
+          <Route path='login' element={<Auth />} />
+          <Route path='*' element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 

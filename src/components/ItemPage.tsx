1078be963c2508f536054719668e979c5aa15dce
cheeseNA <kurocat2000@gmail.com>
2023-03-06 22:02:24 +0900

    Update ItemPage.tsx to use useQueryItem

--- src/components/ItemPage.tsx
+++ src/components/ItemPage.tsx
@@ -1,6 +1,20 @@
 import { useParams } from 'react-router-dom';
+import { useQueryItem } from '../hooks/useQueryItem';
 
 export const ItemPage = () => {
-  const params = useParams();
-  return <div>{params.id}</div>;
+  const { id } = useParams<{ id: string }>();
+  if (typeof id === undefined) {
+    return <div> Id not specified </div>;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const { data: dataItem, isLoading: isLoadingItem } = useQueryItem(id!);
+  return (
+    <div>
+      {isLoadingItem ? (
+        <p>Loading...</p>
+      ) : (
+        <div>{dataItem ? dataItem.title : 'Item Not Found'}</div>
+      )}
+    </div>
+  );
 };

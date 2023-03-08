1f1b99bc22c2b182fd1742549841cfdc2a5367e9
cheeseNA <kurocat2000@gmail.com>
2023-03-08 23:16:44 +0900

    Update redirect behavior of FirebaseSignIn

--- src/components/FirebaseSignIn.tsx
+++ src/components/FirebaseSignIn.tsx
@@ -1,14 +1,23 @@
 import { auth } from '../firebase';
 import { GoogleAuthProvider, GithubAuthProvider, EmailAuthProvider } from 'firebase/auth';
 import StyledFirebaseAuth from './StyledFirebaseAuth';
-
-const uiConfig = {
-  signInFlow: 'popup',
-  signInSuccessUrl: '/',
-  signInOptions: [EmailAuthProvider.PROVIDER_ID, GoogleAuthProvider.PROVIDER_ID, GithubAuthProvider.PROVIDER_ID],
-};
+import { useNavigate } from 'react-router-dom';
 
 export const FirebaseSignIn = () => {
+  const navigate = useNavigate();
+
+  const uiConfig = {
+    signInFlow: 'popup',
+    callbacks: {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      signInSuccessWithAuthResult: (_: any) => {
+        navigate('/');
+        return false;
+      },
+    },
+    signInOptions: [EmailAuthProvider.PROVIDER_ID, GoogleAuthProvider.PROVIDER_ID, GithubAuthProvider.PROVIDER_ID],
+  };
+
   return (
     <div>
       <p>Please sign-in:</p>
